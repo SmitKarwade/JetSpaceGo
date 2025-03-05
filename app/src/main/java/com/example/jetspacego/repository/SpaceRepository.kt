d32122d424ca9978dd5bbf64275798041bab3c6e
SmitKarwade <smitkarwade31@gmail.com>
2025-03-05 18:11:19 +0530

    Deleted repository

--- app/src/main/java/com/example/jetspacego/repository/SpaceRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jetspacego.repository
-
-import com.example.jetspacego.request.SpaceService
-import javax.inject.Inject
-
-class SpaceRepository @Inject constructor(val spaceService: SpaceService){
-    suspend fun getMissions(limit : Int, offset : Int) = spaceService.getMissions(limit, offset)
-}
