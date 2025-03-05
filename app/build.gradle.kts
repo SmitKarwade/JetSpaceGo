aa483dbede4f12e2b59a50f6ef6497f58e4184e0
SmitKarwade <smitkarwade31@gmail.com>
2025-03-05 18:11:37 +0530

    Added paging dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
 
     implementation(libs.compose)
 
+    implementation("androidx.paging:paging-compose:3.3.6")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
