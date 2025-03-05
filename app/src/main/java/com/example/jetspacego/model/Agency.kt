c0922525903cfdded128c6404fc9e3dd2f74fd8d
SmitKarwade <smitkarwade31@gmail.com>
2025-03-05 18:12:00 +0530

    Removed unnecessary fields

--- app/src/main/java/com/example/jetspacego/model/Agency.kt
+++ app/src/main/java/com/example/jetspacego/model/Agency.kt
@@ -5,9 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Agency(
     @SerializedName("abbrev") @Expose val abbrev: String,
-    @SerializedName("id") @Expose val id: Int,
     @SerializedName("name") @Expose val name: String,
-    @SerializedName("response_mode") @Expose val responseMode: String,
     @SerializedName("type") @Expose val type: TypeXX,
     @SerializedName("url") @Expose val url: String
 )
