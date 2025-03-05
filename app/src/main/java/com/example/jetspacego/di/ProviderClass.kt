964d10176dce3da066be52f99371148d27253e45
SmitKarwade <smitkarwade31@gmail.com>
2025-03-05 14:35:55 +0530

    Hilt dependency provider class setup

---
+++ app/src/main/java/com/example/jetspacego/di/ProviderClass.kt
@@ -0,0 +1,32 @@
+package com.example.jetspacego.di
+
+import com.example.jetspacego.constants.Constants
+import com.example.jetspacego.request.SpaceService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProviderClass {
+
+    @Provides
+    @Singleton
+    fun provideSpaceService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSpaceServiceInstance(retrofit: Retrofit): SpaceService {
+        return retrofit.create(SpaceService::class.java)
+    }
+
+}
