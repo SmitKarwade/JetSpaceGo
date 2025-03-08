fdc667056cc346ef749e9a2c74f20101452468d7
SmitKarwade <smitkarwade31@gmail.com>
2025-03-08 10:09:14 +0530

    Added google services plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.dagger.hilt.android") version "2.55" apply false
     kotlin("kapt") version "2.0.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
