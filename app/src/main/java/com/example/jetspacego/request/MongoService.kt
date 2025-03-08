902a2443b36601898abd86ac1cbd4d33f5bac4f5
SmitKarwade <smitkarwade31@gmail.com>
2025-03-08 10:14:20 +0530

    Added interface to make post request for database access

---
+++ app/src/main/java/com/example/jetspacego/request/MongoService.kt
@@ -0,0 +1,23 @@
+package com.example.jetspacego.request
+
+import com.example.jetspacego.model.BookingDetails
+import com.example.jetspacego.model.paymentmodel.OrderResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface MongoService {
+
+    @POST("form/add")
+    suspend fun createdUser(@Body user : BookingDetails)
+
+    @POST("api/payment/create-order")
+    suspend fun createOrder(@Query("amount") amount: Double): OrderResponse
+
+    @POST("api/payment/verify-payment")
+    suspend fun verifyPayment(@Query("paymentId") paymentId: String,
+                              @Query("orderId") orderId: String,
+                              @Query("signature") signature: String): String
+
+
+}
