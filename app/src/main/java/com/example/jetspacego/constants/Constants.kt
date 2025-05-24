a497ffe4ecbb3dffa254c51f25d560e41da4e34c
SmitKarwade <smitkarwade31@gmail.com>
2025-05-24 14:59:50 +0530

    Changed base url

--- app/src/main/java/com/example/jetspacego/constants/Constants.kt
+++ app/src/main/java/com/example/jetspacego/constants/Constants.kt
@@ -1,6 +1,9 @@
 package com.example.jetspacego.constants
 
 object Constants {
+//    val BASE_URL = "https://lldev.thespacedevs.com/2.3.0/"
+//    val BASE_URL_API = "http://127.0.0.1:8080/"
+
     val BASE_URL = "https://lldev.thespacedevs.com/2.3.0/"
-    val BASE_URL_API = "http://127.0.0.1:8080/"
+    val BASE_URL_API = "https://paymentspace-1.onrender.com/"
 }
