765400c200b99ca3dbd48c6c04b2ee58f99843f1
SmitKarwade <smitkarwade31@gmail.com>
2025-06-07 15:41:44 +0530

    Created screen with all mission

--- app/src/main/java/com/example/jetspacego/screens/main/MissionList.kt
+++ app/src/main/java/com/example/jetspacego/screens/main/MissionList.kt
@@ -1,6 +1,6 @@
 package com.example.jetspacego.screens.main
 
-import android.widget.Toast
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,16 +9,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +31,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -34,6 +39,9 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.example.jetspacego.model.launches.Results
 import com.example.jetspacego.paging.SpaceViewModel
+import kotlinx.coroutines.delay
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun MissionList(navController: NavController){
@@ -47,21 +55,47 @@ fun DisplayMission(navController: NavController, viewModel: SpaceViewModel = hil
     val missionFlow = remember(name) { viewModel.getLaunchFlow(name) }
     val displayItems = missionFlow.collectAsLazyPagingItems()
 
-    if (displayItems.itemCount != 0){
+    val isLoading = displayItems.loadState.refresh is androidx.paging.LoadState.Loading
+    val isError = displayItems.loadState.refresh is androidx.paging.LoadState.Error
+    val isEmpty = displayItems.itemCount == 0 && !isLoading && !isError
+
+    if (isEmpty) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "No mission found", style = MaterialTheme.typography.titleMedium)
+        }
+    } else {
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             modifier = Modifier.fillMaxSize()
         ) {
-            items(displayItems.itemCount) { index ->
-                displayItems[index]?.let {
-                    MissionCard(it) {
-                        navController.currentBackStackEntry
-                            ?.savedStateHandle
-                            ?.set("msn", it)
-                        navController.navigate("details")
+            items(
+                count = displayItems.itemCount,
+                key = { index -> displayItems[index]?.id ?: index },
+                span = { index ->
+                    if (index == 0) GridItemSpan(2) else GridItemSpan(1)
+                }
+            ) { index ->
+                val mission = displayItems[index]
+                mission?.let {
+                    if (index == 0) {
+                        CountdownMissionCard(mission = it) {
+                            navController.currentBackStackEntry
+                                ?.savedStateHandle
+                                ?.set("msn", it)
+                            navController.navigate("details")
+                        }
+                    } else {
+                        MissionCard(mission = it) {
+                            navController.currentBackStackEntry
+                                ?.savedStateHandle
+                                ?.set("msn", it)
+                            navController.navigate("details")
+                        }
                     }
                 }
             }
+
+
 
             displayItems.apply {
                 when {
@@ -90,16 +124,9 @@ fun DisplayMission(navController: NavController, viewModel: SpaceViewModel = hil
                 }
             }
         }
-    }else{
-        Box(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                text = "No missions found",
-                modifier = Modifier.align(Alignment.Center)
-            )
-        }
-
     }
 }
+
 
 
 @OptIn(ExperimentalGlideComposeApi::class)
@@ -123,7 +150,7 @@ fun MissionCard(mission: Results, onItemClick : () -> Unit) {
                         contentDescription = it1,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(300.dp)
+                            .height(275.dp)
                             .clip(RoundedCornerShape(16.dp)),
                         contentScale = ContentScale.Crop
                     )
@@ -136,6 +163,8 @@ fun MissionCard(mission: Results, onItemClick : () -> Unit) {
                 Text(
                     text = it,
                     fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                     fontSize = 16.sp
                 )
             }
@@ -144,7 +173,163 @@ fun MissionCard(mission: Results, onItemClick : () -> Unit) {
                 Text(
                     text = it,
                     fontSize = 14.sp,
-                    color = Color.Gray,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            mission.net?.let {
+                Text(
+                    text = "Launch Date",
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            mission.net?.let {
+                Text(
+                    text = "${getMissionLaunchDate(it)}",
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Box(modifier = Modifier.border(1.dp, Color.Gray, RoundedCornerShape(4.dp)).align(Alignment.Start)) {
+                Text(text = getMissionStatus(mission.net.toString()), color = Color.Gray,
+                    fontSize = 12.sp)
+            }
+        }
+    }
+}
+
+
+fun getMissionStatus(net: String): String {
+    return try {
+        val formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME
+        val launchTime = ZonedDateTime.parse(net, formatter)
+        val currentTime = ZonedDateTime.now()
+
+        if (launchTime.isAfter(currentTime)) {
+            "Upcoming"
+        } else {
+            "Past"
+        }
+    } catch (e: Exception) {
+        "Unknown"
+    }
+}
+
+fun getMissionLaunchDate(net: String): String {
+    return try {
+        val formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME
+        val launchTime = ZonedDateTime.parse(net, formatter)
+
+        return launchTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    } catch (e: Exception) {
+        "Unknown"
+    }
+}
+
+@Composable
+fun CountdownMissionCard(mission: Results, onItemClick: () -> Unit) {
+    val formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME
+    val launchTime = remember {
+        try {
+            ZonedDateTime.parse(mission.net, formatter)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    var remainingTime by remember { mutableStateOf("") }
+
+
+    LaunchedEffect(launchTime) {
+        while (true) {
+            launchTime?.let {
+                val now = ZonedDateTime.now()
+                val duration = java.time.Duration.between(now, launchTime)
+                if (!duration.isNegative) {
+                    val hours = duration.toHours()
+                    val minutes = duration.toMinutes() % 60
+                    val seconds = duration.seconds % 60
+                    remainingTime = String.format("%02dh %02dm %02ds", hours, minutes, seconds)
+                } else {
+                    remainingTime = "Launched"
+                }
+            }
+            delay(1000L)
+        }
+    }
+
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        onClick = { onItemClick() },
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            mission.image?.imageUrl?.let {
+                mission.name?.let { it1 ->
+                    GlideImage(
+                        model = it,
+                        contentDescription = it1,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp)
+                            .clip(RoundedCornerShape(12.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(14.dp))
+            Text(
+                text = "Next launch in",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "$remainingTime",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Red,
+                fontSize = 28.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+            Text(
+                text = mission.name ?: "Unknown",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+
+            Text(
+                text = mission.launchServiceProvider?.name ?: "",
+                fontSize = 14.sp,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            mission.net?.let {
+                Text(
+                    text = "Launch Date",
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            mission.net?.let {
+                Text(
+                    text = "${getMissionLaunchDate(it)}",
+                    fontSize = 14.sp,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
                 )
@@ -152,6 +337,8 @@ fun MissionCard(mission: Results, onItemClick : () -> Unit) {
         }
     }
 }
+
+
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
