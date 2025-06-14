679ef6785683de130cf556ca225a0134c0d94a90
SmitKarwade <smitkarwade31@gmail.com>
2025-06-14 15:19:42 +0530

    model class added

---
+++ app/src/main/java/com/example/jetspacego/model/MissionFilterType.kt
@@ -0,0 +1,8 @@
+package com.example.jetspacego.model
+
+enum class MissionFilterType(val displayName: String) {
+    ALL("All"),
+    UPCOMING("Upcoming"),
+    PAST("Past"),
+    NEXT("Next Launch")
+}
---
+++ app/src/main/java/com/example/jetspacego/model/OrgView.kt
@@ -0,0 +1,4 @@
+package com.example.jetspacego.model
+
+data class OrgView(val name: String?, val image: Int?) {
+}
